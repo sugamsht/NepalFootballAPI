3001b3bb8718a904dc2868b70cafd20391ddf900
Sugam Shrestha <sugamsht143@gmail.com>
2021-11-08 13:40:47 +0545

    Player Shema added, stat shema updated

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,6 @@
 /* eslint-disable prettier/prettier */
 'use strict';
 
-
 /**
  * Module dependencies.
  */
@@ -9,6 +8,7 @@
 const home = require('../app/controllers/home');
 const mongoose = require('mongoose');
 var cors = require('cors');
+const alert = require('alert');
 
 var corsOptions = {
   origin: 'http://localhost:5000',
@@ -21,12 +21,13 @@ var corsOptions = {
 
 module.exports = function (app) {
 
+  //Schemas
   let statSchema = new mongoose.Schema({
     team_name: { type: String, required: true },
-    team_position: Number,
     win: Number,
     lost: Number,
-    draw: Number
+    draw: Number,
+    gd: Number,
   });
 
   let fixtureSchema = new mongoose.Schema({
@@ -37,8 +38,18 @@ module.exports = function (app) {
     time: String
   });
 
+  let playerSchema = new mongoose.Schema({
+    fname: { type: String, required: true },
+    lname: { type: String, required: true },
+    dob: { type: Date, default: Date.now },
+    position: { type: String, required: true },
+    jersey_no: { type: Number }
+  });
+
+  //Models
   let Stats = mongoose.model('Stats', statSchema);
   let fixtures = mongoose.model('fixtures', fixtureSchema);
+  let players = mongoose.model('players', playerSchema);
 
   app.get('/', home.index);
 
@@ -55,20 +66,27 @@ module.exports = function (app) {
           return res.json(arrayOfResults)
         }
       }
-    ).sort('team_position')
+    ).sort([['points', -1]])
   });
 
   app.post('/api/test', function (req, res) {
+    var win = parseInt(req.body.win);
+    var draw = parseInt(req.body.draw);
+    var points = (win * 3) + draw;
     let newStat = new Stats({
       team_name: req.body.team_name,
-      team_position: req.body.team_position,
-      win: req.body.win || 0,
+      win: win || 0,
       lost: req.body.lost || 0,
-      draw: req.body.draw || 0
+      draw: draw || 0,
+      gd: req.body.gd || 0,
+      points: points || 0
     });
     newStat.save((error, savedStat) => {
       if (!error && savedStat) {
-        res.json(savedStat);
+        alert('Sucess')
+        //reset form
+        res.redirect('/');
+        // res.json(savedStat);
       }
     });
   });
@@ -91,6 +109,33 @@ module.exports = function (app) {
 
   app.get('/api/fixtures', cors(corsOptions), function (req, res) {
     fixtures.find({},
+      (error, arrayOfResults) => {
+        if (!error && arrayOfResults) {
+          return res.json(arrayOfResults)
+        }
+      }
+    )
+  });
+
+  //players halne
+  app.post('/api/players', function (req, res) {
+    let newPlayer = new players({
+      fname: req.body.fname,
+      lname: req.body.lname,
+      dob: req.body.dob,
+      position: req.body.position,
+      jersey_no: req.body.jersey_no
+    });
+    newPlayer.save((error, savedPlayer) => {
+      if (!error && savedPlayer) {
+        res.json(savedPlayer);
+      }
+    });
+  })
+
+  //players get
+  app.get('/api/players', cors(corsOptions), function (req, res) {
+    players.find({},
       (error, arrayOfResults) => {
         if (!error && arrayOfResults) {
           return res.json(arrayOfResults)
