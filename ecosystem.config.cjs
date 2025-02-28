d5c81fade850414cd8fd05e053695f70dc890e17
Sugam <sugamsht143@gmail.com>
2025-02-28 14:14:13 +0545

    Add environment variable configurations for development and production in ecosystem config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,6 +7,17 @@ module.exports = {
     instances: 1,       // Fork mode runs a single instance per app
     autorestart: true,
     watch: false,
-    max_memory_restart: '1G'
+    max_memory_restart: '1G',
+    env: { // Default environment variables (used if NODE_ENV is not explicitly set)
+      NODE_ENV: 'development', // Or any other default you want
+    },
+    env_production: { // Production environment variables
+      NODE_ENV: 'production',
+      MONGODB_URL: process.env.MONGODB_URL,
+      SESSION_SECRET: process.env.SESSION_SECRET,
+      SALT: process.env.SALT,
+      Backend_URL: process.env.Backend_URL,
+      FRONTEND_URL: process.env.FRONTEND_URL,
+    }
   }]
-};
+}
